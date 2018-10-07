6b2e839dc8de81b1458bfa28ba6eefb1e68f476e
Shobhit Jain <shobhitjain@Shobhits-MacBook-Pro.local>
2018-10-07 19:49:19 +0900

    added initial functions

---
+++ README.md
@@ -0,0 +1,2 @@
+# helper_functions
+This repository contains some commonly used helper functions. It's mainly for personal use but you are welcome to utilise this repository if you don't want to spend your worthless time on worthwhile things.
---
+++ nlp_helper_funcs.py
@@ -0,0 +1,51 @@
+# Converts the unicode file to ascii
+def unicode_to_ascii(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn')
+
+
+def preprocess_sentence(w):
+    w = unicode_to_ascii(w.lower().strip())
+    
+    # creating a space between a word and the punctuation following it
+    # eg: "he is a boy." => "he is a boy ." 
+    # Reference:- https://stackoverflow.com/questions/3645931/python-padding-punctuation-with-white-spaces-keeping-punctuation
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)
+    
+    # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+    w = re.sub(r"[^a-zA-Z?.!,¿]+", " ", w)
+    
+    w = w.rstrip().strip()
+    
+    # adding a start and an end token to the sentence
+    # so that the model know when to start and stop predicting.
+    w = '<start> ' + w + ' <end>'
+    return w
+
+# This class creates a word -> index mapping (e.g,. "dad" -> 5) and vice-versa 
+# (e.g., 5 -> "dad") for each language,
+class LanguageIndex():
+  def __init__(self, lang):
+    self.lang = lang
+    self.word2idx = {}
+    self.idx2word = {}
+    self.vocab = set()
+    
+    self.create_index()
+    
+  def create_index(self):
+    for phrase in self.lang:
+      self.vocab.update(phrase.split(' '))
+    
+    self.vocab = sorted(self.vocab)
+    
+    self.word2idx['<pad>'] = 0
+    for index, word in enumerate(self.vocab):
+      self.word2idx[word] = index + 1
+    
+    for word, index in self.word2idx.items():
+      self.idx2word[index] = word
+
+dataset = tf.data.Dataset.from_tensor_slices((input_tensor_train, target_tensor_train)).shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
